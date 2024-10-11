5cf5bf23f048db51b67da053c29920631a517e8d
Jakub Niewelt <jakubniewelt@icloud.com>
2024-10-11 17:40:43 +0200

    feat: add + configure i18n

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
-import { Button } from "antd";
+import { useTranslation } from "react-i18next";
+import { Button, Select } from "antd";
+
+const langs = { en: "English", pl: "Polski" };
 
 function App() {
-  return <Button>test</Button>;
+  const { t, i18n } = useTranslation();
+  return (
+    <>
+      <Select
+        defaultValue={i18n.language}
+        onChange={(lng) => i18n.changeLanguage(lng)}
+        options={Object.keys(langs).map((lng) => {
+          return { value: lng, label: langs[lng] };
+        })}
+      />
+      <Button>{t("test")}</Button>
+    </>
+  );
 }
 
 export default App;
