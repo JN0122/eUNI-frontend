a58f5525367f5b6b6dfefd7528cd8a3cc3e80d4b
Jakub Niewelt <jakubniewelt@icloud.com>
2024-10-25 19:57:03 +0200

    feat: save user in localstorage

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -4,13 +4,14 @@ import {loginUser} from "../api/api.js";
 const AuthContext = createContext();
 
 export function AuthProvider({ children }){
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
 
     async function login(userData) {
         const response = await loginUser(userData);
 
         if(response?.status === 200){
             setUser(response.data);
+            localStorage.setItem("user", JSON.stringify(response.data));
         }else{
             console.error(`${response.status}: ${response.data}`);
         }
