453616faf1f645bdbb0a949afa640d7636272ba6
Jakub Niewelt <jakubniewelt@icloud.com>
2024-10-25 19:48:07 +0200

    feat: store authtoken in httpOnly cookies

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = "http://localhost:5104";
+axios.defaults.withCredentials = true;
 
 export const loginUser = async (credentials) => {
     try {
