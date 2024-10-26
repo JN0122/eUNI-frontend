211779b30364bf5ccbefc2c219b5cd62fc1d1dd9
Jakub Niewelt <jakubniewelt@icloud.com>
2024-10-26 15:49:02 +0200

    feat: loading submit button, display error when logging in

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,9 @@
 import AppLayout from "../components/layout/AppLayout.jsx";
-import { Button, Checkbox, Flex, Form, Input, theme } from "antd";
+import { Alert, Button, Checkbox, Flex, Form, Input, theme } from "antd";
 import { LockOutlined, UserOutlined } from "@ant-design/icons";
 import { useAuth } from "../context/AuthContext.jsx";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 function Login() {
@@ -13,6 +13,8 @@ function Login() {
     const { login, user } = useAuth();
     const navigate = useNavigate();
     const { t } = useTranslation();
+    const [error, setError] = useState(null);
+    const [submitLoading, setSubmitLoading] = useState(false);
 
     useEffect(() => {
         if (user !== null) {
@@ -21,21 +23,22 @@ function Login() {
     }, [user, navigate]);
 
     async function onFinish(values) {
+        setSubmitLoading(true);
         const response = await login({
             email: values.email,
             password: values.password,
         });
-        if (response?.status === 200) {
+        setSubmitLoading(false);
+        if (response.status === 200) {
             navigate("/dashboard");
+        } else {
+            setError(response);
         }
     }
 
     return (
         <AppLayout centerChildren={true} showHeaderLogo={true}>
-            <Form
-                name="login"
-                initialValues={{ remember: true }}
-                onFinish={onFinish}
+            <div
                 style={{
                     width: "30rem",
                     padding: "3em",
@@ -43,51 +46,74 @@ function Login() {
                     borderRadius: borderRadiusLG,
                 }}
             >
-                <Form.Item
-                    name="email"
-                    rules={[
-                        {
-                            required: true,
-                            message: t("error-input-email"),
-                        },
-                    ]}
-                >
-                    <Input prefix={<UserOutlined />} placeholder={t("email")} />
-                </Form.Item>
-                <Form.Item
-                    name="password"
-                    rules={[
-                        {
-                            required: true,
-                            message: t("error-input-password"),
-                        },
-                    ]}
-                >
-                    <Input.Password
-                        prefix={<LockOutlined />}
-                        type="password"
-                        placeholder={t("password")}
+                {error && (
+                    <Alert
+                        message={`${t("error")} ${error.status || ""}`}
+                        description={`${t(error.data)}`}
+                        type="error"
+                        showIcon
+                        style={{ marginBottom: "1rem" }}
                     />
-                </Form.Item>
-                <Form.Item>
-                    <Flex justify="space-between" align="center">
-                        <Form.Item
-                            name="remember"
-                            valuePropName="checked"
-                            noStyle
-                        >
-                            <Checkbox>{t("remember-me")}</Checkbox>
-                        </Form.Item>
-                        <a href="">{t("forgot-password")}</a>
-                    </Flex>
-                </Form.Item>
+                )}
+                <Form
+                    name="login"
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                >
+                    <Form.Item
+                        name="email"
+                        rules={[
+                            {
+                                required: true,
+                                message: t("error-input-email"),
+                            },
+                        ]}
+                    >
+                        <Input
+                            prefix={<UserOutlined />}
+                            placeholder={t("email")}
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: t("error-input-password"),
+                            },
+                        ]}
+                    >
+                        <Input.Password
+                            prefix={<LockOutlined />}
+                            type="password"
+                            placeholder={t("password")}
+                        />
+                    </Form.Item>
+                    <Form.Item>
+                        <Flex justify="space-between" align="center">
+                            <Form.Item
+                                name="remember"
+                                valuePropName="checked"
+                                noStyle
+                            >
+                                <Checkbox>{t("remember-me")}</Checkbox>
+                            </Form.Item>
+                            <a href="">{t("forgot-password")}</a>
+                        </Flex>
+                    </Form.Item>
 
-                <Form.Item>
-                    <Button block type="primary" htmlType="submit">
-                        {t("login")}
-                    </Button>
-                </Form.Item>
-            </Form>
+                    <Form.Item>
+                        <Button
+                            block
+                            type="primary"
+                            htmlType="submit"
+                            loading={submitLoading}
+                        >
+                            {t("login")}
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
         </AppLayout>
     );
 }
