250015c3cd1495c42a6bf29ff2aa83e06536961c
Jakub Niewelt <jakubniewelt@icloud.com>
2024-10-26 16:52:37 +0200

    fix: validate email

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -48,7 +48,7 @@ function Login() {
             >
                 {error && (
                     <Alert
-                        message={`${t("error")} ${error.status || ""}`}
+                        message={`${t("error")}`}
                         description={`${t(error.data)}`}
                         type="error"
                         showIcon
@@ -64,6 +64,8 @@ function Login() {
                         name="email"
                         rules={[
                             {
+                                pattern:
+                                    "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])",
                                 required: true,
                                 message: t("error-input-email"),
                             },
