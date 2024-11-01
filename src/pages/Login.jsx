43268bf281a902b81591655339cec9d7458a53df
Jakub Niewelt <jakubniewelt@icloud.com>
2024-11-01 19:33:32 +0100

    refactor: reuse translation items, change pattern to type in email input

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -76,10 +76,9 @@ function Login() {
                         name="email"
                         rules={[
                             {
-                                pattern:
-                                    "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])",
+                                type: "email",
                                 required: true,
-                                message: t("error-input-email"),
+                                message: t("error-correct-email-is-required"),
                             },
                         ]}
                     >
@@ -93,7 +92,7 @@ function Login() {
                         rules={[
                             {
                                 required: true,
-                                message: t("error-input-password"),
+                                message: t("error-password-is-required"),
                             },
                         ]}
                     >
