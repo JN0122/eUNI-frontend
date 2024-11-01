1338d1fb9f501ae104feb2c59e084c62a374889e
Jakub Niewelt <jakubniewelt@icloud.com>
2024-11-01 14:25:51 +0100

    feat: profile page menu items: user-info, change-password, change-email

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,41 @@
 import ContentBlock from "../components/layout/ContentBlock.jsx";
+import SiderMenu from "../components/layout/SiderMenu.jsx";
+import { LockOutlined, MailOutlined, UserOutlined } from "@ant-design/icons";
+import { useTranslation } from "react-i18next";
+import { Outlet } from "react-router-dom";
 
 function Profile() {
+    const { t } = useTranslation();
+
+    let items = [
+        {
+            label: t("basic-info"),
+            key: 1,
+            icon: <UserOutlined />,
+        },
+        {
+            label: t("change-password"),
+            key: 2,
+            icon: <LockOutlined />,
+        },
+        {
+            label: t("change-email"),
+            key: 3,
+            icon: <MailOutlined />,
+        },
+    ];
     return (
-        <ContentBlock>
-            <h1>Siemano</h1>
+        <ContentBlock
+            breadcrumbs={[{ title: t("Profil") }]}
+            menu={
+                <SiderMenu
+                    defaultOpenKeys={["sub2"]}
+                    fullHeight={true}
+                    items={items}
+                />
+            }
+        >
+            <Outlet />
         </ContentBlock>
     );
 }
