3009334c3768ba640b66feee596fb4bed47020b1
Jakub Niewelt <jakubniewelt@icloud.com>
2024-11-01 14:16:44 +0100

    refactor: remove parse functions

--- src/components/layout/Breadcrumbs.jsx
+++ src/components/layout/Breadcrumbs.jsx
@@ -1,12 +1,5 @@
 import { Breadcrumb } from "antd";
 
-export function parseBreadcrumbItem(title, path) {
-    return {
-        title,
-        path,
-    };
-}
-
 function Breadcrumbs({ ...rest }) {
     return <Breadcrumb style={{ marginTop: "1em" }} {...rest} />;
 }
--- src/components/layout/SiderMenu.jsx
+++ src/components/layout/SiderMenu.jsx
@@ -1,14 +1,5 @@
 import { Menu } from "antd";
 
-export function parseMenuItem(label, key, icon, children) {
-    return {
-        key,
-        icon,
-        children,
-        label,
-    };
-}
-
 function SiderMenu({ fullHeight, ...rest }) {
     return (
         <Menu
