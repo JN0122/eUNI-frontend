69fde509a80aa2924fe789eab6dfb89dfe740cb8
Jakub Niewelt <jakubniewelt@icloud.com>
2024-11-15 23:07:49 +0100

    refactor: move table with actions to separate component

---
+++ src/components/TableWithActions.jsx
@@ -0,0 +1,268 @@
+import { useTranslation } from "react-i18next";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { App, Button, Flex, Input, Skeleton, Space, Table } from "antd";
+import { ExclamationCircleFilled, SearchOutlined } from "@ant-design/icons";
+import getNotificationConfig from "../helpers/getNotificationConfig.js";
+import { useDrawer } from "../context/DrawerContext.jsx";
+
+const getColumnSortProps = (dataIndex) => {
+    return {
+        sorter: (a, b) =>
+            a[dataIndex].charCodeAt(0) - b[dataIndex].charCodeAt(0),
+        sortDirections: ["descend", "ascend"],
+        showSorterTooltip: null
+    };
+};
+
+function TableWithActions({
+    columns,
+    fetchData,
+    modalConfirmContent,
+    onModalConfirm,
+    notificationSuccessText
+}) {
+    const { t } = useTranslation();
+    const { openDrawer, setData, data } = useDrawer();
+    const [dataSource, setDataSource] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const searchInput = useRef(null);
+    const { notification, modal } = App.useApp();
+
+    const getDataSource = useCallback(
+        async function () {
+            setLoading(true);
+            const response = await fetchData();
+
+            if (response.status !== 200)
+                return notification.error(
+                    getNotificationConfig(t("error-could-not-connect-to-api"))
+                );
+            setDataSource(
+                response.data.map((user) => {
+                    const { id, ...rest } = user;
+                    return {
+                        ...rest,
+                        key: id
+                    };
+                })
+            );
+            setLoading(false);
+        },
+        [fetchData, notification, t]
+    );
+
+    useEffect(() => {
+        getDataSource();
+    }, [getDataSource, data]);
+
+    const handleOk = useCallback(
+        async function (record) {
+            try {
+                await onModalConfirm(record);
+                await getDataSource();
+                notification.success(
+                    getNotificationConfig(notificationSuccessText)
+                );
+            } catch {
+                notification.error(
+                    getNotificationConfig(t("error-unexpected"))
+                );
+            }
+        },
+        [
+            getDataSource,
+            notification,
+            notificationSuccessText,
+            onModalConfirm,
+            t
+        ]
+    );
+
+    const showDeleteConfirm = useCallback(
+        function (record) {
+            modal.confirm({
+                title: t("delete-are-you-sure"),
+                icon: <ExclamationCircleFilled />,
+                content: modalConfirmContent(record),
+                okText: t("delete"),
+                okType: "danger",
+                cancelText: t("cancel"),
+                onOk() {
+                    handleOk(record);
+                },
+                onCancel() {}
+            });
+        },
+        [handleOk, modal, modalConfirmContent, t]
+    );
+
+    const handleSearch = (selectedKeys, confirm) => {
+        confirm();
+    };
+
+    const handleReset = (clearFilters, confirm) => {
+        clearFilters();
+        confirm();
+    };
+
+    const getColumnSearchProps = useCallback(
+        (dataIndex, searchInputText) => ({
+            filterDropdown: ({
+                setSelectedKeys,
+                selectedKeys,
+                confirm,
+                clearFilters
+            }) => (
+                <div
+                    style={{
+                        padding: 8
+                    }}
+                    onKeyDown={(e) => e.stopPropagation()}
+                >
+                    <Input
+                        ref={searchInput}
+                        placeholder={searchInputText}
+                        value={selectedKeys[0]}
+                        onChange={(e) =>
+                            setSelectedKeys(
+                                e.target.value ? [e.target.value] : []
+                            )
+                        }
+                        onPressEnter={() =>
+                            handleSearch(selectedKeys, confirm, dataIndex)
+                        }
+                        style={{
+                            marginBottom: 8,
+                            display: "block"
+                        }}
+                    />
+                    <Space>
+                        <Button
+                            type="primary"
+                            onClick={() =>
+                                handleSearch(selectedKeys, confirm, dataIndex)
+                            }
+                            icon={<SearchOutlined />}
+                            size="small"
+                            style={{
+                                width: 90
+                            }}
+                        >
+                            {t("search")}
+                        </Button>
+                        <Button
+                            onClick={() =>
+                                clearFilters &&
+                                handleReset(clearFilters, confirm)
+                            }
+                            size="small"
+                            style={{
+                                width: 90
+                            }}
+                        >
+                            {t("reset")}
+                        </Button>
+                    </Space>
+                </div>
+            ),
+            filterIcon: (filtered) => (
+                <SearchOutlined
+                    style={{
+                        color: filtered ? "#1677ff" : undefined
+                    }}
+                />
+            ),
+            onFilter: (value, record) =>
+                record[dataIndex]
+                    .toString()
+                    .toLowerCase()
+                    .includes(value.toLowerCase()),
+            filterDropdownProps: {
+                onOpenChange(open) {
+                    if (open) {
+                        setTimeout(() => searchInput.current?.select(), 100);
+                    }
+                }
+            }
+        }),
+        [t]
+    );
+
+    const parsedColumns = useMemo(() => {
+        const newColumns = columns.map((column) => {
+            let additionalProps = {};
+            if (column?.withSearch)
+                additionalProps = {
+                    ...additionalProps,
+                    ...getColumnSearchProps(
+                        column.dataIndex,
+                        column?.searchInputText
+                    )
+                };
+            if (column?.withSort)
+                additionalProps = {
+                    ...additionalProps,
+                    ...getColumnSortProps(column.dataIndex)
+                };
+            return {
+                title: column.title,
+                dataIndex: column.dataIndex,
+                ...additionalProps
+            };
+        });
+        newColumns.push({
+            title: t("actions"),
+            key: "action",
+            render: (_, record) => (
+                <Space size="middle">
+                    <a
+                        onClick={() => {
+                            openDrawer();
+                            setData({
+                                ...record,
+                                newPassword: "",
+                                repeatNewPassword: ""
+                            });
+                        }}
+                    >
+                        {t("edit")}
+                    </a>
+                    <a
+                        style={{ color: "red" }}
+                        onClick={() => showDeleteConfirm(record)}
+                    >
+                        {t("delete")}
+                    </a>
+                </Space>
+            )
+        });
+        return newColumns;
+    }, [
+        columns,
+        getColumnSearchProps,
+        openDrawer,
+        setData,
+        showDeleteConfirm,
+        t
+    ]);
+
+    if (dataSource?.length === 0) {
+        return <Skeleton loading />;
+    }
+    return (
+        <>
+            <Flex gap="middle" vertical>
+                <Table
+                    columns={parsedColumns}
+                    loading={loading}
+                    dataSource={dataSource}
+                    scroll={{
+                        x: "max-content"
+                    }}
+                />
+            </Flex>
+        </>
+    );
+}
+
+export default TableWithActions;
--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -1,196 +1,26 @@
 import ContentBlock from "../../components/ContentBlock.jsx";
 import { useTranslation } from "react-i18next";
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
-import {
-    Button,
-    Flex,
-    Input,
-    Modal,
-    notification,
-    Skeleton,
-    Space,
-    Table,
-    Typography
-} from "antd";
-import { ExclamationCircleFilled, SearchOutlined } from "@ant-design/icons";
+import { useMemo } from "react";
+import { Typography } from "antd";
 import { deleteUser, getAllUsers } from "../../api/users.js";
-import getNotificationConfig from "../../helpers/getNotificationConfig.js";
 import EditUserDrawer from "./EditUserDrawer.jsx";
-import { useDrawer } from "../../context/DrawerContext.jsx";
+import TableWithActions from "../../components/TableWithActions.jsx";
 
-const { confirm } = Modal;
 const { Text } = Typography;
 
 function Users() {
     const { t } = useTranslation();
-    const { openDrawer, setData, data } = useDrawer();
-    const [searchText, setSearchText] = useState("");
-    const [searchedColumn, setSearchedColumn] = useState("");
-    const searchInput = useRef(null);
 
-    const [dataSource, setDataSource] = useState([]);
-
-    const fetchAllUsers = useCallback(
-        function () {
-            getAllUsers().then((users) => {
-                setDataSource(
-                    users.data.map((user) => {
-                        return {
-                            key: user.id,
-                            firstName: user.firstName,
-                            lastName: user.lastName,
-                            email: user.email
-                        };
-                    })
-                );
-            });
-        },
-        [setDataSource]
+    const modalConfirmContent = (record) => (
+        <>
+            {t("first-name")}: <Text strong>{record.firstName}</Text> <br />
+            {t("last-name")}: <Text strong>{record.lastName}</Text> <br />
+            {t("email")}: <Text strong>{record.email}</Text> <br />
+        </>
     );
 
-    useEffect(() => {
-        fetchAllUsers();
-    }, [fetchAllUsers, data]);
-
-    const removeUser = useCallback(
-        async function (record) {
-            try {
-                await deleteUser(record.key);
-                fetchAllUsers();
-                notification.success(
-                    getNotificationConfig(t("success-remove-user"))
-                );
-            } catch {
-                notification.error(
-                    getNotificationConfig(t("error-unexpected"))
-                );
-            }
-        },
-        [fetchAllUsers, t]
-    );
-
-    const showDeleteConfirm = useCallback(
-        async function (record) {
-            confirm({
-                title: t("are-you-sure-you-want-to-remove-user"),
-                icon: <ExclamationCircleFilled />,
-                content: (
-                    <>
-                        {t("first-name")}:{" "}
-                        <Text strong>{record.firstName}</Text> <br />
-                        {t("last-name")}: <Text strong>{record.lastName}</Text>{" "}
-                        <br />
-                        {t("email")}: <Text strong>{record.email}</Text> <br />
-                    </>
-                ),
-                okText: t("delete"),
-                okType: "danger",
-                cancelText: t("cancel"),
-                async onOk() {
-                    await removeUser(record);
-                },
-                onCancel() {}
-            });
-        },
-        [t, removeUser]
-    );
-
-    const handleSearch = (selectedKeys, confirm, dataIndex) => {
-        confirm();
-        setSearchText(selectedKeys[0]);
-        setSearchedColumn(dataIndex);
-    };
-
-    const handleReset = (clearFilters, confirm) => {
-        clearFilters();
-        setSearchText("");
-        confirm();
-    };
-
-    const getColumnSearchProps = (dataIndex, searchInputText) => ({
-        filterDropdown: ({
-            setSelectedKeys,
-            selectedKeys,
-            confirm,
-            clearFilters
-        }) => (
-            <div
-                style={{
-                    padding: 8
-                }}
-                onKeyDown={(e) => e.stopPropagation()}
-            >
-                <Input
-                    ref={searchInput}
-                    placeholder={searchInputText}
-                    value={selectedKeys[0]}
-                    onChange={(e) =>
-                        setSelectedKeys(e.target.value ? [e.target.value] : [])
-                    }
-                    onPressEnter={() =>
-                        handleSearch(selectedKeys, confirm, dataIndex)
-                    }
-                    style={{
-                        marginBottom: 8,
-                        display: "block"
-                    }}
-                />
-                <Space>
-                    <Button
-                        type="primary"
-                        onClick={() =>
-                            handleSearch(selectedKeys, confirm, dataIndex)
-                        }
-                        icon={<SearchOutlined />}
-                        size="small"
-                        style={{
-                            width: 90
-                        }}
-                    >
-                        {t("search")}
-                    </Button>
-                    <Button
-                        onClick={() =>
-                            clearFilters && handleReset(clearFilters, confirm)
-                        }
-                        size="small"
-                        style={{
-                            width: 90
-                        }}
-                    >
-                        {t("reset")}
-                    </Button>
-                </Space>
-            </div>
-        ),
-        filterIcon: (filtered) => (
-            <SearchOutlined
-                style={{
-                    color: filtered ? "#1677ff" : undefined
-                }}
-            />
-        ),
-        onFilter: (value, record) =>
-            record[dataIndex]
-                .toString()
-                .toLowerCase()
-                .includes(value.toLowerCase()),
-        filterDropdownProps: {
-            onOpenChange(open) {
-                if (open) {
-                    setTimeout(() => searchInput.current?.select(), 100);
-                }
-            }
-        }
-    });
-
-    const getColumnSortProps = (dataIndex) => {
-        return {
-            sorter: (a, b) =>
-                a[dataIndex].charCodeAt(0) - b[dataIndex].charCodeAt(0),
-            sortDirections: ["descend", "ascend"],
-            showSorterTooltip: null
-        };
+    const removeUser = async function (record) {
+        await deleteUser(record.key);
     };
 
     const columns = useMemo(
@@ -202,62 +32,30 @@ function Users() {
             {
                 title: t("last-name"),
                 dataIndex: "lastName",
-                ...getColumnSearchProps("lastName", t("enter-last-name"))
+                withSearch: true,
+                searchInputText: t("enter-last-name")
             },
             {
                 title: t("email"),
                 dataIndex: "email",
-                ...getColumnSearchProps("email", t("enter-email")),
-                ...getColumnSortProps("email")
-            },
-            {
-                title: t("actions"),
-                key: "action",
-                render: (_, record) => (
-                    <Space size="middle">
-                        <a
-                            onClick={() => {
-                                openDrawer();
-                                setData({
-                                    ...record,
-                                    newPassword: "",
-                                    repeatNewPassword: ""
-                                });
-                            }}
-                        >
-                            {t("edit")}
-                        </a>
-                        <a
-                            style={{ color: "red" }}
-                            onClick={() => showDeleteConfirm(record)}
-                        >
-                            {t("delete")}
-                        </a>
-                    </Space>
-                )
+                withSearch: true,
+                searchInputText: t("enter-email"),
+                withSort: true
             }
         ],
-        [t, getColumnSearchProps, openDrawer, setData, showDeleteConfirm]
+        [t]
     );
 
     return (
         <ContentBlock breadcrumbs={[{ title: t("users") }]}>
-            {dataSource.length === 0 ? (
-                <Skeleton loading />
-            ) : (
-                <>
-                    <EditUserDrawer />
-                    <Flex gap="middle" vertical>
-                        <Table
-                            columns={columns}
-                            dataSource={dataSource}
-                            scroll={{
-                                x: "max-content"
-                            }}
-                        />
-                    </Flex>
-                </>
-            )}
+            <EditUserDrawer />
+            <TableWithActions
+                columns={columns}
+                fetchData={getAllUsers}
+                modalConfirmContent={modalConfirmContent}
+                onModalConfirm={removeUser}
+                notificationSuccessText={t("success-remove-user")}
+            />
         </ContentBlock>
     );
 }
