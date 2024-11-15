8dcb055adccb26844d2ddf1dd24db869dc35a46e
Jakub Niewelt <jakubniewelt@icloud.com>
2024-11-15 19:50:04 +0100

    feat: add search and sort to columns

--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -1,16 +1,18 @@
 import ContentBlock from "../../components/ContentBlock.jsx";
 import { useTranslation } from "react-i18next";
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import {
+    Button,
     Flex,
+    Input,
     Modal,
     notification,
     Skeleton,
     Space,
     Table,
-    Typography,
+    Typography
 } from "antd";
-import { ExclamationCircleFilled } from "@ant-design/icons";
+import { ExclamationCircleFilled, SearchOutlined } from "@ant-design/icons";
 import { deleteUser, getAllUsers } from "../../api/users.js";
 import getNotificationConfig from "../../helpers/getNotificationConfig.js";
 import EditUserDrawer from "./EditUserDrawer.jsx";
@@ -22,6 +24,9 @@ const { Text } = Typography;
 function Users() {
     const { t } = useTranslation();
     const { openDrawer, setData, data } = useDrawer();
+    const [searchText, setSearchText] = useState("");
+    const [searchedColumn, setSearchedColumn] = useState("");
+    const searchInput = useRef(null);
 
     const [dataSource, setDataSource] = useState([]);
 
@@ -34,13 +39,13 @@ function Users() {
                             key: user.id,
                             firstName: user.firstName,
                             lastName: user.lastName,
-                            email: user.email,
+                            email: user.email
                         };
-                    }),
+                    })
                 );
             });
         },
-        [setDataSource],
+        [setDataSource]
     );
 
     useEffect(() => {
@@ -53,15 +58,15 @@ function Users() {
                 await deleteUser(record.key);
                 fetchAllUsers();
                 notification.success(
-                    getNotificationConfig(t("success-remove-user")),
+                    getNotificationConfig(t("success-remove-user"))
                 );
             } catch {
                 notification.error(
-                    getNotificationConfig(t("error-unexpected")),
+                    getNotificationConfig(t("error-unexpected"))
                 );
             }
         },
-        [fetchAllUsers, t],
+        [fetchAllUsers, t]
     );
 
     const showDeleteConfirm = useCallback(
@@ -84,25 +89,126 @@ function Users() {
                 async onOk() {
                     await removeUser(record);
                 },
-                onCancel() {},
+                onCancel() {}
             });
         },
-        [t, removeUser],
+        [t, removeUser]
     );
+
+    const handleSearch = (selectedKeys, confirm, dataIndex) => {
+        confirm();
+        setSearchText(selectedKeys[0]);
+        setSearchedColumn(dataIndex);
+    };
+
+    const handleReset = (clearFilters, confirm) => {
+        clearFilters();
+        setSearchText("");
+        confirm();
+    };
+
+    const getColumnSearchProps = (dataIndex, searchInputText) => ({
+        filterDropdown: ({
+            setSelectedKeys,
+            selectedKeys,
+            confirm,
+            clearFilters
+        }) => (
+            <div
+                style={{
+                    padding: 8
+                }}
+                onKeyDown={(e) => e.stopPropagation()}
+            >
+                <Input
+                    ref={searchInput}
+                    placeholder={searchInputText}
+                    value={selectedKeys[0]}
+                    onChange={(e) =>
+                        setSelectedKeys(e.target.value ? [e.target.value] : [])
+                    }
+                    onPressEnter={() =>
+                        handleSearch(selectedKeys, confirm, dataIndex)
+                    }
+                    style={{
+                        marginBottom: 8,
+                        display: "block"
+                    }}
+                />
+                <Space>
+                    <Button
+                        type="primary"
+                        onClick={() =>
+                            handleSearch(selectedKeys, confirm, dataIndex)
+                        }
+                        icon={<SearchOutlined />}
+                        size="small"
+                        style={{
+                            width: 90
+                        }}
+                    >
+                        {t("search")}
+                    </Button>
+                    <Button
+                        onClick={() =>
+                            clearFilters && handleReset(clearFilters, confirm)
+                        }
+                        size="small"
+                        style={{
+                            width: 90
+                        }}
+                    >
+                        {t("reset")}
+                    </Button>
+                </Space>
+            </div>
+        ),
+        filterIcon: (filtered) => (
+            <SearchOutlined
+                style={{
+                    color: filtered ? "#1677ff" : undefined
+                }}
+            />
+        ),
+        onFilter: (value, record) =>
+            record[dataIndex]
+                .toString()
+                .toLowerCase()
+                .includes(value.toLowerCase()),
+        filterDropdownProps: {
+            onOpenChange(open) {
+                if (open) {
+                    setTimeout(() => searchInput.current?.select(), 100);
+                }
+            }
+        }
+    });
+
+    const getColumnSortProps = (dataIndex) => {
+        return {
+            sorter: (a, b) =>
+                a[dataIndex].charCodeAt(0) - b[dataIndex].charCodeAt(0),
+            sortDirections: ["descend", "ascend"],
+            showSorterTooltip: null
+        };
+    };
 
     const columns = useMemo(
         () => [
             {
                 title: t("first-name"),
-                dataIndex: "firstName",
+                dataIndex: "firstName"
             },
             {
                 title: t("last-name"),
                 dataIndex: "lastName",
+                ...getColumnSearchProps("lastName", t("enter-last-name"))
             },
             {
                 title: t("email"),
                 dataIndex: "email",
+                ...getColumnSearchProps("email", t("enter-email")),
+                ...getColumnSortProps("email")
             },
             {
                 title: t("actions"),
@@ -112,7 +218,11 @@ function Users() {
                         <a
                             onClick={() => {
                                 openDrawer();
-                                setData(record);
+                                setData({
+                                    ...record,
+                                    newPassword: "",
+                                    repeatNewPassword: ""
+                                });
                             }}
                         >
                             {t("edit")}
@@ -124,10 +234,10 @@ function Users() {
                             {t("delete")}
                         </a>
                     </Space>
-                ),
-            },
+                )
+            }
         ],
-        [t, showDeleteConfirm],
+        [t, getColumnSearchProps, openDrawer, setData, showDeleteConfirm]
     );
 
     return (
@@ -142,7 +252,7 @@ function Users() {
                             columns={columns}
                             dataSource={dataSource}
                             scroll={{
-                                x: "max-content",
+                                x: "max-content"
                             }}
                         />
                     </Flex>
