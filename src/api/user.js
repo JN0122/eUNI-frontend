eec67e9c01cfbe42de3280fb13ff34b14d285f4e
Jakub Niewelt <jakubniewelt@icloud.com>
2024-11-22 18:31:12 +0100

    fix: get user info from new endpoint

--- src/api/user.js
+++ src/api/user.js
@@ -1,13 +1,13 @@
 import axiosInstance from "./axios.js";
 
 export async function getUserData() {
-    return await axiosInstance.get(`/api/User/user-info`);
+    return await axiosInstance.get(`/api/User/info`);
 }
 
 export async function changeEmail(data) {
-    return await axiosInstance.patch(`/api/User/change-email`, data);
+    return await axiosInstance.patch(`/api/User/email`, data);
 }
 
 export async function changePassword(data) {
-    return await axiosInstance.patch(`/api/User/change-password`, data);
+    return await axiosInstance.patch(`/api/User/password`, data);
 }
