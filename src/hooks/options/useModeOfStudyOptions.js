2ec33241c7ee1c43dd822d394ac04b283cd039be
Jakub Niewelt <jakubniewelt@icloud.com>
2024-12-13 00:03:14 +0100

    refactor: use one type of study translation

--- src/hooks/options/useModeOfStudyOptions.js
+++ src/hooks/options/useModeOfStudyOptions.js
@@ -3,11 +3,11 @@ import { useMemo } from "react";
 
 export default function useModeOfStudyOptions() {
     const { t } = useTranslation();
-    
+
     return useMemo(() => {
         return [
-            { value: "true", label: t("studies-are-full-time") },
-            { value: "false", label: t("studies-are-part-time") }
+            { value: "true", label: t("full-time-field-of-study") },
+            { value: "false", label: t("part-time-field-of-study") }
         ];
     }, [t]);
 }
